0b9aad8bec67bdc6c6772ebc15e029d75432ce5c
Franz <pfquispe@gmail.com>
2020-03-15 17:52:18 -0400

    Added files

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: 'http://localhost:9000'
         })
     }
 }
